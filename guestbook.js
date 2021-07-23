6f9f41dfb26aa703132a6032e2ef3cce80000fa8
kevinmbuicode <77851451+kevinmbuicode@users.noreply.github.com>
2021-07-23 21:28:25 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f322859f.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
